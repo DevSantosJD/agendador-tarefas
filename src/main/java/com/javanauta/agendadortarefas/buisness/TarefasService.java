27968e8c38ae801f905e851e759deb335e40398d
Jardel Santos <jardel.santos.23@outlook.com>
2026-07-11 14:00:32 -0300

    adicionados métodos Get por email e por peiodo

--- src/main/java/com/javanauta/agendadortarefas/buisness/TarefasService.java
+++ src/main/java/com/javanauta/agendadortarefas/buisness/TarefasService.java
@@ -7,9 +7,11 @@ import com.javanauta.agendadortarefas.infrastructure.enums.StatusNotificacaoEnum
 import com.javanauta.agendadortarefas.infrastructure.repository.TarefasRepository;
 import com.javanauta.agendadortarefas.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -30,5 +32,21 @@ public class TarefasService {
 
         return tarefaConverter.paraTerafaDTO(
                 tarefasRepository.save(entity));
+    }
+
+    public List<TarefasDTO> buscaTarefasAgendadasPorPeriodo(LocalDateTime dataInicial, LocalDateTime dataFinal){
+
+        return tarefaConverter.paraListTarefasDTO(
+                tarefasRepository.findByDataEventoBetween(dataInicial, dataFinal));
+
+    }
+
+    //Retorna lista de tarefas por email
+    public List<TarefasDTO> buscaTarefasPorEmail(String token){
+        String email = jwtUtil.extractEmailToken(token.substring(7));
+
+        List<TarefasEntity> listaTarefas = tarefasRepository.findByEmailUsuario(email);
+
+        return tarefaConverter.paraListTarefasDTO(listaTarefas);
     }
 }
--- src/main/java/com/javanauta/agendadortarefas/buisness/mapper/TarefasConverter.java
+++ src/main/java/com/javanauta/agendadortarefas/buisness/mapper/TarefasConverter.java
@@ -4,10 +4,18 @@ import com.javanauta.agendadortarefas.buisness.dto.TarefasDTO;
 import com.javanauta.agendadortarefas.infrastructure.entity.TarefasEntity;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface TarefasConverter {
 
     TarefasEntity paraTarefaEntity(TarefasDTO dto);
 
     TarefasDTO paraTerafaDTO(TarefasEntity entity);
+
+    List<TarefasEntity> paraListaTarefasEntit (List<TarefasDTO> dtos);
+
+   List<TarefasDTO> paraListTarefasDTO (List<TarefasEntity> tarefasEntityList);
+
+
 }
